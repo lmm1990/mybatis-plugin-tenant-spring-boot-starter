19c9ac6d03ba20cb38cb052c9e46d5b52d446215
liumingming <mingming.liu@dataxgroup.com>
2021-09-06 14:18:08 +0800

    first commit

---
+++ mybatis.plugin.tenant.demo/src/main/java/mybatisPluginTenantSpringBootStarterDemo/mapper/TestMapper.java
@@ -0,0 +1,35 @@
+package mybatisPluginTenantSpringBootStarterDemo.mapper;
+
+import com.github.lmm1990.mybatis.plugin.tenant.spring.boot.starter.annotation.IgnoreTenantField;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface TestMapper {
+
+    /**
+     * 添加
+     */
+    @IgnoreTenantField
+    int add(String name);
+
+    /**
+     * 修改
+     */
+    int update(@Param("name") String name,@Param("id") int id);
+
+    /**
+     * 删除
+     */
+    int delete(int id);
+
+    /**
+     * 列表
+     */
+    @Select("SELECT * FROM test")
+    List<Map> list();
+}
---
+++ spring-boot-starter/src/main/java/com/github/lmm1990/mybatis/plugin/tenant/spring/boot/starter/handler/TenantDataHandler.java
@@ -0,0 +1,44 @@
+package com.github.lmm1990.mybatis.plugin.tenant.spring.boot.starter.handler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 公共数据
+ *
+ * @author liumingming
+ * @since 2021-09-02 16:37
+ */
+public class TenantDataHandler {
+
+    /**
+     * 租户值
+     */
+    private static final ThreadLocal<Object> TENANT_FIELD_VALUE = new ThreadLocal<>();
+
+
+    /**
+     * 忽略租户字段方法列表
+     */
+    public static final Set<String> ignoreTenantfieldMethods = new HashSet<>();
+
+    /**
+     * 获得租户值
+     *
+     * @return java.lang.Object
+     * @since 刘明明/2021-09-02 16:41:29
+     **/
+    public static Object getTenantFieldValue() {
+        return TENANT_FIELD_VALUE.get();
+    }
+
+    /**
+     * 设置租户值
+     *
+     * @param tenantFieldValue: 租户值
+     * @since 刘明明/2021-09-02 16:41:52
+     **/
+    public static void setTenantFieldValue(Object tenantFieldValue) {
+        TenantDataHandler.TENANT_FIELD_VALUE.set(tenantFieldValue);
+    }
+}
